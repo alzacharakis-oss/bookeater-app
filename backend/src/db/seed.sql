9e1cbbde7f3fb7984aa58233b02e7255b838910f
al.zach <al.zacharakis@gmail.com>
2026-08-21 23:40:42 +0300

    feat: Added seed data (10 books for testing purposes)

---
+++ backend/src/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (title, author) VALUES
+  ('1984', 'George Orwell'),
+  ('Animal Farm', 'George Orwell'),
+  ('The Name of the Rose', 'Umberto Eco'),
+  ('The Lord of the Rings', 'J.R.R. Tolkien'),
+  ('Brave New World', 'Aldous Huxley'),
+  ('Fahrenheit 451', 'Ray Bradbury'),
+  ('The Great Gatsby', 'F. Scott Fitzgerald'),
+  ('To Kill a Mockingbird', 'Harper Lee'),
+  ('Crime and Punishment', 'Fyodor Dostoevsky'),
+  ('One Hundred Years of Solitude', 'Gabriel García Márquez');
